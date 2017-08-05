259c8abcb6e2aa7bea1ecbff517c5d4f33b110c9
parchk <565625207@qq.com>
2017-08-05 10:55:36 +0800

    remove all

--- README.md
+++
@@ -1,1 +0,0 @@
-ugonfs is a user-space implementation of the NFSv3 server specification
--- conf/conf_test.go
+++
@@ -1,9 +0,0 @@
-package conf
-
-import (
-	"testing"
-)
-
-func Test_LoadExport(t *testing.T) {
-	LoadExports()
-}
